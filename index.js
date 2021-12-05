8a1f264018fdea4569b792c4271aeb0c794e23d2
bora001 <bora.vancouver@gmail.com>
2021-12-05 14:07:20 -0800

    Get current location

--- index.js
+++ index.js
@@ -2,9 +2,12 @@ const success = (pos) => {
   let crd = pos.coords;
   let lat = crd.latitude;
   let lon = crd.longitude;
-  console.log(crd.latitude);
-  console.log(crd.longitude);
-  //   console.log(pos, "pos");
+
+  const locationNow = fetch(`https://geocode.xyz/${lat},${lon}?json=1"`)
+    .then((res) => res.json())
+    .then((data) => console.log(data));
+
+  console.log(locationNow);
 };
 
 const error = (err) => {

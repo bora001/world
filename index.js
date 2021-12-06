deaf68190044f3fc461af22ba1372cf213dfe6f0
bora001 <bora.vancouver@gmail.com>
2021-12-06 13:31:57 -0800

    render country info

--- index.js
+++ index.js
@@ -20,11 +20,44 @@ const error = (err) => {
 navigator.geolocation.getCurrentPosition(success, error);
 
 const countryInfo = (country) => {
-  console.log(country, "this is!");
-
   const info = fetch(`https://restcountries.com/v2/name/${country}`).then(
-    (res) => res.json().then((data) => console.log(data))
+    (res) =>
+      res
+        .json()
+        .then((data) => {
+          console.log(data);
+          let [info] = data;
+          RenderCountry(info);
+          if (info.borders) {
+            countryInfo(info.borders.toString());
+          }
+        })
+        .catch((err) => console.log(err))
   );
-
-  // https://restcountries.com/v3.1/name/{name}
 };
+const cnt = document.getElementById("cnt");
+
+const RenderCountry = (info) => {
+  const html = `<div class="country_box">
+      <div class="flag_box"><img class="flag" src=${
+        info.flags.png
+      } alt="" /></div>
+      <div class="txt_box">
+        <h1 class="name">${info.name}</h1>
+        <p class="capital">${info.capital}</p>
+        <p class="lang">${info.languages.map((a) => a.name)}</p>
+        <p class="currency">${info.currencies.map((a) =>
+          Object.values(a).join(" ")
+        )}</p>
+      </div>
+    </div>
+    `;
+  cnt.insertAdjacentHTML("beforeend", html);
+};
+
+const getInfoBtn = document.querySelector(".getinfo");
+getInfoBtn.addEventListener("click", function () {
+  //   console.log(cnt);
+  cnt.style.opacity = 1;
+  this.remove();
+});
